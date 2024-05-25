f705110691a39ad427b80f9f615555e95a5a8e63
yassinkr <kerraiyassin@gmail.com>
2024-05-25 21:06:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/header"
+const App = () => {
+  return (
+    <div className="w-full overflow-x-hidden h-screen">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
