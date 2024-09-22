5981d303f2a7942c05d25cf3f0055dd7da206a3f
yassinkr <kerraiyassin@gmail.com>
2024-09-22 21:00:57 +0100

    responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header className='w-full h-fit flex justify-around items-end bg-white top-0'>
      <Logo/>
      <Nav/>
-     <Button className='mb-3'>
+     <Button className='mb-3 hidden md:block'>
         Contribute
      </Button>
     </header>
